6575126d4a05b1e4615e4bd57dad19c7f880c796
dhyang22 <35789171+dhyang22@users.noreply.github.com>
2018-05-17 11:53:20 +0530

    Create ch2.ex2.py

---
+++ python/Chapter2/Excercise/Problems/ch2.ex2.py
@@ -0,0 +1,14 @@
+
+# Excercise 2
+# ------------
+# In a script, an expression all by itself is a legal statement, but it doesn’t do anything. The script
+# 17 
+# 3.2 
+# "Hello, World!" 
+# 1 + 1
+# produces no output at all. How would you change the script to display the values of these four expressions?
+
+17 
+3.2 
+"Hello, World!" 
+1 + 1
